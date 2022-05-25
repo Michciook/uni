5ece9f5f9746b0910f67cf3e887d9a68c1785b06
Oskar Michta <michtabiznes@gmail.com>
2022-05-25 17:34:01 +0000

    Fixed "jump" parameter, so the spaces between each number is even

--- c13-SqrtFunction.c
+++ c13-SqrtFunction.c
@@ -1,41 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void arrayCreator(int x1, int x2, int xArr[], int size) {
+void arrayCreator(int x1, int x2, double xArr[], int size) {
     int range;
     if(x1 > 0 && x2 > 0 || x1 < 0 && x2 < 0) {
         range = abs(x1) - abs(x2);
     } else {
-        range = abs(x1) + abs(x2)+1;
+        range = abs(x1) + abs(x2);
     }
-    int jump = abs(range)/size;
-    for(int i=0; i < abs(range)/jump-1; i++) {
+    double jump = (double)abs(range)/(size-1);
+    for(int i=0; i < abs(range)/jump; i++) {
         xArr[i] = x1 + i*jump;
     }
     xArr[size-1] = x2;
 }
 
-squareFunction(int a, int b, int c, int arr[], int size) {
-    int y;
+void squareFunction(int a, int b, int c, double arr[], int size) {
+    double y;
     FILE *fp;
     fp = fopen("tekst.csv", "w+");
     for(int i=0; i<size; i++) {
-        y = arr[i]*arr[i]*a+b*arr[i]+c;
-        fprintf(fp, "%d\t", arr[i]);
-        fprintf(fp, "%d\n", y);
+        y = (double)arr[i]*arr[i]*a+b*arr[i]+c;
+        fprintf(fp, "%f\t", arr[i]);
+        fprintf(fp, "%f\n", y);
     }
     fclose(fp);
 }
 
-void main() {
-    int x1,x2,x;
-    printf("Ile chcesz wyswietlic liczb: ");
-    scanf("%d", &x);
-    int xArr[x];
+int main() {
+    int x1,x2,x=0;
+    while(x<=0) {
+        printf("Ile chcesz wyswietlic liczb: ");
+        scanf("%d", &x);
+        if(x<=0) {
+            printf("Podaj poprawną liczbę\n");
+        }
+    }
+    double xArr[x];
     printf("Podaj x1: ");
     scanf("%d", &x1);
     printf("Podaj x2: ");
     scanf("%d", &x2);
     arrayCreator(x1, x2, xArr, x);
     squareFunction(-1,1,1,xArr, x);
+    return 0;
 }
