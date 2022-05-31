ab5bc67371c4faf5e61bef12ecb01c47c33bc346
Oskar Michta <michtabiznes@gmail.com>
2022-05-31 08:56:50 +0000

    c15-ListOfStudents.c edited online with Bitbucket

--- c15-ListOfStudents.c
+++ c15-ListOfStudents.c
@@ -37,27 +37,70 @@ void pobierzZLewej(struct ListaStudentow** lista) {
     }
 }
 
-void pobierz(struct ListaStudentow** lista, int szukana) {
+int wyszukajZListy(struct ListaStudentow** lista, int liczbaPoszukiwana) {
     if(*lista == 0) {
-        printf("Lista pusta ...\n");
+        printf("Lista pusta...\n");
+        return -1;
     } else {
-        struct ListaStudentow* tmp = *lista;
-        int wartoscDoZwrotu = 0;
-        while(wartoscDoZwrotu == szukana) {
-            *lista = tmp->nastepnyElement;
-            wartoscDoZwrotu = (*lista)->wartosc;
-        }
+        struct ListaStudentow* worm = *lista;
 
-        printf("Numer indexu: %d\nImie: %s\nNazwisko: %s\n", wartoscDoZwrotu, (*lista)->imie, (*lista)->nazwisko);
+        while(worm != 0) {
+            if(worm->wartosc == liczbaPoszukiwana) {
+                printf("Numer indexu: %d\nImie: %s\nNazwisko: %s\n", worm->wartosc, worm->imie, worm->nazwisko);
+                return 1;
+            }
+            worm = worm->nastepnyElement;
+        }
+        printf("Brak szukanego studenta\n");
+        return 0;
+
     }
 }
 
+int usun(struct ListaStudentow ** lista, int wartosc) {
+    struct ListaStudentow *previous, *current;
+
+    if (*lista == NULL) {
+        return -1;
+    }
+
+    if ((*lista)->wartosc == wartosc) {
+        struct ListaStudentow* tmp = *lista;
+        *lista = tmp->nastepnyElement;
+        free(tmp);
+        return 1;
+    }
+
+    previous = *lista;
+    current = (*lista)->nastepnyElement;
+    while (current) {
+        if (current->wartosc == wartosc) {
+            previous->nastepnyElement = current->nastepnyElement;
+            free(current);
+            return wartosc;
+        }
+
+        previous = current;
+        current  = current->nastepnyElement;
+    }
+    return -1;
+}
+
+
 int main(int argc, char *argv[]) {
     struct ListaStudentow * lista = 0;
-    int c,id;
+    int c,id,ids;
     char im[20], nz[20];
+    dodaj(&lista, 1, "aa", "aa");
+    dodaj(&lista, 2, "bb", "bb");
+    dodaj(&lista, 3, "cc", "cc");
     while(1) {
-        printf("Co chcesz zrobic:\n1 - Dodac studenta\n2 - Wyswietlic dane ostatniego studenta\n0 - zakonczyc program\n");
+        printf("Co chcesz zrobic:"
+               "\n1 - Dodac studenta\n"
+               "2 - Wyswietlic dane ostatniego studenta\n"
+               "3 - Wyszukac studenta po indeksie\n"
+               "4 - Usunac studenta o danym indeksie\n"
+               "0 - zakonczyc program\n");
         scanf("%d", &c);
         switch (c) {
             case 1:
@@ -73,7 +116,14 @@ int main(int argc, char *argv[]) {
                 pobierzZLewej(&lista);
                 break;
             case 3:
-                pobierz(&lista, 2);
+                printf("Podaj numer indeksu studenta ktorego poszukujesz: ");
+                scanf("%d", &ids);
+                wyszukajZListy(&lista, ids);
+                break;
+            case 4:
+                printf("Studenta o jakim indeksie chcesz usunac: ");
+                scanf("%d", &ids);
+                usun(&lista, ids);
                 break;
             case 0:
                 return 0;
